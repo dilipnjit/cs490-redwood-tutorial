22e4da26cd949dcb21b98024f196fc4e5c64f5e6
vap49 <vap49@njit.edu>
2022-10-04 10:27:35 -0400

    added comments

---
+++ redwood-tutorial-main/web/src/components/CommentsCell/CommentsCell.mock.js
@@ -0,0 +1,16 @@
+export const standard = () => ({
+  comments: [
+    {
+      id: 1,
+      name: 'Villaire',
+      body: 'First comment',
+      createdAt: '2020-01-02T12:34:56Z',
+    },
+    {
+      id: 2,
+      name: 'Pierre',
+      body: 'Second comment',
+      createdAt: '2020-02-03T23:00:00Z',
+    },
+  ],
+})
