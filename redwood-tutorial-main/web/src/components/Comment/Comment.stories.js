4dea48b4820aabf66e55488943ffc3febd4814b9
WillisDo <wd83@njit.edu>
2022-10-04 11:17:49 -0400

    finished chapter 7, role based authentication, now moderators can delete comments, default role is mod

--- redwood-tutorial-main/web/src/components/Comment/Comment.stories.js
+++ redwood-tutorial-main/web/src/components/Comment/Comment.stories.js
@@ -1,14 +1,37 @@
 import Comment from './Comment'
 
-export const generated = () => {
+export const defaultView = () => {
   return (
-    <Comment
-      comment={{
-        name: 'Villaire',
-        body: 'This is the first comment!',
-        createdAt: '2020-01-01T12:34:56Z'
-      }}
-    />
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1
+        }}
+      />
+    </div>
+  )
+}
+
+export const moderatorView = () => {
+  mockCurrentUser({
+    roles: 'moderator',
+  })
+  return (
+    <div className="m-4">
+      <Comment
+        comment={{
+          id: 1,
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+          postId: 1,
+        }}
+      />
+    </div>
   )
 }
 
